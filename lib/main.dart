0879c8902f22ac0ba09ae9fecc945eff4c45fa73
ayurish2399 <ayurish.chandna_cs18@gla.ac.in>
2021-05-20 08:41:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:todo_list/screens/todo_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Todo list',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: TodoListScreeen(),
+    );
+  }
+}
